a39834eaa9ba991f14a058f80a9de7c725dc9651
artiom sokolov <asokolik87@gmail.com>
2021-05-03 16:57:38 +0300

    footer media query

--- src/components/blocks/footer/Footer.js
+++ src/components/blocks/footer/Footer.js
@@ -11,40 +11,66 @@ import ArrowBottom from './../../../assets/img/ArrowBottom.svg';
 function Footer() {
     return (
         <div className={s.footer}>
-            <div className={s.container}>
                 <div className={s.footerTop}>
-                    <img src={logo} alt="" className={s.logo}/>
-                    <div className={s.socials}>
-                        <a href="#" className={s.socialsLink}>
-                            <img src={VK} alt="vk" className={s.vk}/>
-                        </a>
-                        <a href="#" className={s.socialsLink}>
-                            <img src={FB} alt="facebook" className={s.facebook}/>
-                        </a>
-                        <a href="#" className={s.socialsLink}>
-                            <img src={INSTAGRAM} alt="instagram" className={s.instagram}/>
-                        </a>
+                    <div className={s.container}>
+                        <div className={s.topInner}>
+                            <img src={logo} alt="" className={s.logo}/>
+                            <div className={s.socials}>
+                                <a href="#" className={s.socialsLink}>
+                                    <img src={VK} alt="vk" className={s.vk}/>
+                                </a>
+                                <a href="#" className={s.socialsLink}>
+                                    <img src={FB} alt="facebook" className={s.facebook}/>
+                                </a>
+                                <a href="#" className={s.socialsLink}>
+                                    <img src={INSTAGRAM} alt="instagram" className={s.instagram}/>
+                                </a>
+                            </div>
+                        </div>
+                        
                     </div>
                 </div>
-                <div className={s.footerBottom}>
-                    <div className={s.footerTopSmall}>
-                        <div className={s.payments}>
-                            <img src={MC} alt="mc" className={s.mc}/>
-                            <img src={VISA} alt="visa" className={s.visa}/>
+                <div className={s.footerBottomMob}>
+                    <div className={s.container}>
+                        <div className={s.footerBottomSmall}>
+                            <div className={s.payments}>
+                                <img src={MC} alt="mc" className={s.mc}/>
+                                <img src={VISA} alt="visa" className={s.visa}/>
+                            </div>
+                            <div className={s.language}>
+                                <span className={s.lan}>RU</span>
+                                <img className={s.arrow} src={ArrowBottom} alt="arrow" />
+                            </div>
                         </div>
-                        <div className={s.language}>
-                            <span className={s.lan}>RU</span>
-                            <img className={s.arrow} src={ArrowBottom} alt="arrow" />
+                        <p className={s.footerText}>
+                            Тех. поддержка: 8(800)700-00-00 <br/>
+                            &copy; 2018 HELYFLY. All Rights Reserved.
+                        </p>
+                    </div>
+                </div>
+
+                <div className={s.footerBottom}>
+                    <div className={s.container}>
+                        <div className={s.inner}>
+                            <div className={s.payments}>
+                                <img src={MC} alt="mc" className={s.mc}/>
+                                <img src={VISA} alt="visa" className={s.visa}/>
+                            </div>
+                            <p className={s.footerText}>
+                                Тех. поддержка: 8(800)700-00-00 <br/>
+                                &copy; 2018 HELYFLY. All Rights Reserved.
+                            </p>
+                            <div className={s.language}>
+                                <span className={s.lan}>RU</span>
+                                <img className={s.arrow} src={ArrowBottom} alt="arrow" />
+                            </div>
                         </div>
                     </div>
-                    <p className={s.footerText}>
-                        Тех. поддержка: 8(800)700-00-00 <br/>
-                        &copy; 2018 HELYFLY. All Rights Reserved.
-                    </p>
                 </div>
                 
-            </div>
+                
         </div>
+    
     );
 }
 
