55a4cabfc48068090620e2716a25bbf3c2e8be4b
artiom sokolov <asokolik87@gmail.com>
2021-05-05 22:02:39 +0300

    package section

--- src/components/blocks/package/Package.js
+++ src/components/blocks/package/Package.js
@@ -9,13 +9,15 @@ import ArrowBottom from './../../../assets/img/ArrowBottom.svg';
 function Package() {
     return (
         <>
-        <div className={s.container}>
-            <h2 className={s.title}>Фотографии пакета</h2>
+
+       
+        <div className={s.package}>
+                <div className={s.container}>
+                    <h2 className={s.title}>Фотографии пакета</h2>
+                </div>
                 <div className={s.packageSlider}>
                     <img className={s.packageImg} src={packagePhotoHely} alt="PhotoHely"/>
                     <img className={s.packageImg} src={packagePhotoHely} alt="PhotoHely"/>                    
-                    <img className={s.packageImg} src={packagePhotoHely} alt="PhotoHely"/>
-                    <img className={s.packageImg} src={packagePhotoHely} alt="PhotoHely"/>
                     <img className={s.packageImg} src={packagePhotoEarth} alt="PhotoEarth"/>
                     <img className={s.packageImg} src={packagePhotoEarth} alt="PhotoEarth"/>
                     <img className={s.packageImg} src={packagePhotoGirl} alt="PhotoGirl"/>
@@ -23,8 +25,8 @@ function Package() {
                     <img className={s.packageImg} src={packagePhotoKiss} alt="PhotoKiss"/>
                     <img className={s.packageImg} src={packagePhotoKiss} alt="PhotoKiss"/>
                 </div>
-                <div className={s.more}>
-                    <div className={s.container}>
+                <div className={s.container}>
+                    <div className={s.more}>
                         <a href="#" className={s.linkReviews}> 
                             <img className={s.arrow} src={ArrowBottom} alt="arrow" />
                             Показать все фото
